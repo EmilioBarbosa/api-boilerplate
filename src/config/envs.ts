54ec7d761a5a3912ba3eafed44ef355b1e5d50e2
Emilio Barbosa <emiliobarbos4@gmail.com>
2024-11-10 20:31:56 -0300

    initial configuration

---
+++ src/config/envs.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+const envSchema = z.object({
+    APP_PORT: z.string()
+})
+
+const env = envSchema.parse(process.env)
+
+export default env
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import Fastify, { FastifyInstance } from "fastify";
+import envs from "./config/envs";
+
+const fastify: FastifyInstance = Fastify({
+  logger: true,
+});
+
+fastify.get("/healthcheck", async function handler(request, reply) {
+  return { status: "Ok" };
+});
+
+async function main() {
+  try {
+    await fastify.listen({ port: parseInt(envs.APP_PORT), host: "0.0.0.0" });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+main();
